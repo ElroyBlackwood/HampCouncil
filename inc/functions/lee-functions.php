32f5d0dcfb0d721aadf37104c48494b72b0453c1
boksinternational <kennyburke@boks-international.com>
2019-05-20 15:33:39 +0100

    header begin creation

--- inc/functions/lee-functions.php
+++ inc/functions/lee-functions.php
@@ -2,7 +2,30 @@
 // lee functions file 
 
 function outputHeader() { ?>
+<header>
 	<div class="container-fluid">
+		<div id="top-header">
+
+		</div>
+		<div class="header-nav">
+			<div class="logo">
+
+			</div>
+			<div class="header-nav-menu" role="navigation">
+				    <?php /* Primary navigation */
+						wp_nav_menu( array(
+						  'theme_location' => 'header-menu',
+						  'depth' => 2,
+						  'container' => false,
+						  'menu_class' => 'nav',
+						  //Process nav menu using our custom nav walker
+						  'walker' => new wp_bootstrap_navwalker())
+						);
+					?>
+			</div>
+		</div>
+	</div>
+</header>
 <?php }
 
 ?>
