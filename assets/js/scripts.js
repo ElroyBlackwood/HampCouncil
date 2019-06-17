3c0c0d888f7c1ca4a10bf727a6e7d239f0ec3519
boksinternational <kennyburke@boks-international.com>
2019-06-17 11:01:32 +0100

    Property Gallery Tweaks

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -549,7 +549,25 @@ $(document).ready(function() {
 	  asNavFor: '.main-image-gallery',
 	  dots: false,
 	  centerMode: true,
-	  focusOnSelect: true
+	  focusOnSelect: true,
+	  responsive: [
+	     {
+	       breakpoint: 1200,
+	       settings: {
+	         slidesToShow: 3,
+	         slidesToScroll: 1,
+	         infinite: true,
+	         dots: true
+	       }
+	     },
+	     {
+	       breakpoint: 600,
+	       settings: {
+	         slidesToShow: 1,
+	         slidesToScroll: 1
+	       }
+	     }
+	   ]
 	});
 
 // ==========================================
